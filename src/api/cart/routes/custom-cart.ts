133171dd49555de5c49e1f263a48da38a9059cfa
Artem Kovtun <rtmkvtn@gmail.com>
2025-05-17 16:07:07 +0800

    feat(cart): add custom GET /cart controller

---
+++ src/api/cart/routes/custom-cart.ts
@@ -0,0 +1,9 @@
+module.exports = {
+  routes: [
+    {
+      method: 'GET',
+      path: '/cart',
+      handler: 'cart.my', // or 'plugin::plugin-name.controllerName.functionName' for a plugin-specific controller
+    },
+  ],
+}
